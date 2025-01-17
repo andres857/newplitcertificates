c43772d5f1ec50fbe3b7f4354cf7e816e5a80eef
Andres <andres857@gmail.com>
2025-01-17 18:10:19 -0500

    fix errors

--- ia.py
+++ ia.py
@@ -1,8 +1,12 @@
 # from google import genai
 # from google.genai import types
+from dotenv import load_dotenv
 from openai import OpenAI
 from typing import Optional, Dict, Any
-import json
+import json, os
+
+load_dotenv()
+
 class InferenceError(Exception):
     """Clase personalizada para errores de inferencia"""
     pass
@@ -21,9 +25,10 @@ class CertificateInfo:
         return f"CertificateInfo(nombre='{self.nombre}', identificacion='{self.identificacion}')"
     
 gemini_key = ''
+api_key = os.getenv('API_OPENAI')
 
 # client = genai.Client(api_key=gemini_key)
-client_open_ai = OpenAI(api_key='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')
+client_open_ai = OpenAI(api_key=api_key)
 
 prompt="Tu tarea es extraer información específica de certificados de cursos y capacitaciones. Instrucciones: 1. Lee cuidadosamente el texto del certificado proporcionado 2. Identifica el nombre completo de la persona certificada 3. Identifica el número de identificación (puede estar precedido por C.C., Cédula, DNI, etc.) 4. Devuelve la información en formato JSON con las siguientes claves: - 'nombre': nombre completo de la persona - 'identificacion': número de identificación (solo números, sin prefijos) Si algún campo no se encuentra, devuelve null para ese campo. Ejemplo de entrada: CERTIFICA A: JUAN CARLOS PEREZ MARTINEZ C.C. 79845632 Por su asistencia... Ejemplo de salida: { 'nombre': 'JUAN CARLOS PEREZ MARTINEZ', 'identificacion': '79845632' } El contendo es diverso si no puedes inferir que se certifica con un nombre como CERTIFICA A: JUAN CARLOS PEREZ MARTINEZ devuelve null ya que no todos los nombres son validos ya que pueden ser el nombre de la intitucion o el nombre del certificado o quien firma pero queremos obtener a quien se otorga y si tiene , el nombre quitala A partir de ahora, procesa el siguiente texto y devuelve solo el JSON:"
 
